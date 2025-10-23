8442aa19c74e38708f3fd1e49e393d595ed453c6
arpit529srivastava <arpitsrivastava529@gmail.com>
2025-10-23 20:33:11 +0530

    Add Terraform configuration and example files
    Signed-off-by: arpit529srivastava <arpitsrivastava529@gmail.com>

---
+++ terraform/variables.tf
@@ -0,0 +1,64 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "hayy"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "hayy-ai-cluster"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.28"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.20.0/24"]
+}
+
+variable "public_access_cidrs" {
+  description = "CIDR blocks for public access to EKS API"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "log_retention_days" {
+  description = "CloudWatch log retention in days"
+  type        = number
+  default     = 7
+}
